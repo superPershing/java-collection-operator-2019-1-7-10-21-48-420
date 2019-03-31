3ea091982e7212bf6b8156f54b40b7d3eb7b2732
superPershing <727342027@qq.com>
2019-04-01 05:16:10 +0800

    feat(FlatenTest): finish FlatenTest.java

--- src/main/java/com/thoughtworks/collection/Flaten.java
+++ src/main/java/com/thoughtworks/collection/Flaten.java
@@ -2,7 +2,9 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Flaten {
 
@@ -12,10 +14,10 @@ public class Flaten {
     }
 
     public List<Integer> transformToOneDimesional() {
-        throw new NotImplementedException();
+        return Arrays.stream(this.array).flatMap(Arrays::stream).collect(Collectors.toList());
     }
 
     public List<Integer> transformToUnrepeatedOneDimesional() {
-        throw new NotImplementedException();
+        return Arrays.stream(this.array).flatMap(Arrays::stream).distinct().collect(Collectors.toList());
     }
 }
