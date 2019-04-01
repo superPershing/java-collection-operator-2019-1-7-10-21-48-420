bcda5bfacd941d52e83db0b9a6fab1a80361a786
superPershing <727342027@qq.com>
2019-04-01 21:06:39 +0800

    feat(MyMap): finish MyMap.java

--- src/main/java/com/thoughtworks/collection/MyMap.java
+++ src/main/java/com/thoughtworks/collection/MyMap.java
@@ -2,8 +2,11 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class MyMap {
 
@@ -17,22 +20,35 @@ public class MyMap {
     }
 
     public List<Integer> getTriple() {
-        throw new NotImplementedException();
+        return this.array.stream().map(i -> i * 3).collect(Collectors.toList());
     }
 
     public List<String> mapLetter() {
-        throw new NotImplementedException();
+        return this.array.stream().map(i -> Character.toString((char) (i.intValue() + 96))).collect(Collectors.toList());
     }
 
     public List<String> mapLetters() {
-        throw new NotImplementedException();
+        return this.array.stream().map(i -> this.transferStringToChars(i, this.letterList)).collect(Collectors.toList());
+    }
+
+    public static String transferStringToChars(Integer num, List<String> letterList) {
+        StringBuilder sb = new StringBuilder();
+        int firstCharIndex = (num - 1) / 26;
+        int secondCharIndex = (num - 1) % 26;
+        if (firstCharIndex == 0) {
+            sb.append(letterList.get(secondCharIndex));
+        } else {
+            sb.append(letterList.get(firstCharIndex - 1));
+            sb.append(letterList.get(secondCharIndex));
+        }
+        return sb.toString();
     }
 
     public List<Integer> sortFromBig() {
-        throw new NotImplementedException();
+        return this.array.stream().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
     }
 
     public List<Integer> sortFromSmall() {
-        throw new NotImplementedException();
+        return this.array.stream().sorted().collect(Collectors.toList());
     }
 }
