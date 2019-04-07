84ae22148f2ebaec2cb827cd6f6baad371deb8d5
superPershing <727342027@qq.com>
2019-04-07 18:20:07 +0800

    refactor(Add): refactor Add.java

--- src/main/java/com/thoughtworks/collection/Add.java
+++ src/main/java/com/thoughtworks/collection/Add.java
@@ -1,31 +1,22 @@
 package com.thoughtworks.collection;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Add {
     public int getSumOfEvens(int leftBorder, int rightBorder) {
         int smallBorder = leftBorder < rightBorder ? leftBorder : rightBorder;
         int bigBorder = rightBorder > leftBorder ? rightBorder : leftBorder;
-        int sumEvens = 0;
-        for (int i = smallBorder; i <= bigBorder; i++) {
-            if (i % 2 == 0) {
-                sumEvens += i;
-            }
-        }
-        return sumEvens;
+        return IntStream.rangeClosed(smallBorder, bigBorder).filter(i -> i % 2 == 0).sum();
     }
 
     public int getSumOfOdds(int leftBorder, int rightBorder) {
         int smallBorder = leftBorder < rightBorder ? leftBorder : rightBorder;
         int bigBorder = rightBorder > leftBorder ? rightBorder : leftBorder;
-        int sumOdds = 0;
-        for (int i = smallBorder; i <= bigBorder; i++) {
-            if (i % 2 == 1) {
-                sumOdds += i;
-            }
-        }
-        return sumOdds;
+        return IntStream.rangeClosed(smallBorder, bigBorder).filter(i -> i % 2 == 1).sum();
     }
 
     public int getSumTripleAndAddTwo(List<Integer> arrayList) {
@@ -33,48 +24,40 @@ public class Add {
     }
 
     public List<Integer> getTripleOfOddAndAddTwo(List<Integer> arrayList) {
-        List<Integer> result = new ArrayList<>();
-        for (Integer i : arrayList) {
-            if (i % 2 == 0) {
-                result.add(i);
-            } else {
-                result.add(i * 3 + 2);
-            }
-        }
-        return result;
+        return arrayList.stream().map(this::processTripleOfOddAndAddTwo).collect(Collectors.toList());
     }
+
+    public int processTripleOfOddAndAddTwo(int i) {
+        if (i % 2 == 0) {
+            return i;
+        }
+        return i * 3 + 2;
+    }
+
 
     public int getSumOfProcessedOdds(List<Integer> arrayList) {
         return arrayList.stream().filter(i -> i % 2 == 1).mapToInt(i -> i * 3 + 5).sum();
     }
 
     public double getMedianOfEven(List<Integer> arrayList) {
-        double result;
         List<Integer> evenList = arrayList.stream().filter(i -> i % 2 == 0).sorted().collect(Collectors.toList());
         if (evenList.size() % 2 == 0) {
-            result = (evenList.get(evenList.size() / 2 - 1) + evenList.get(evenList.size() / 2)) / 2.0;
-        } else {
-            result = evenList.get(evenList.size() / 2);
+            return (evenList.get(evenList.size() / 2 - 1) + evenList.get(evenList.size() / 2)) / 2.0;
         }
-        return result;
+        return evenList.get(evenList.size() / 2);
     }
 
     public double getAverageOfEven(List<Integer> arrayList) {
-        return arrayList.stream().filter(i -> i % 2 == 0).mapToDouble(i -> i.doubleValue()).average().getAsDouble();
+        return arrayList.stream().filter(i -> i % 2 == 0).mapToDouble(Integer::doubleValue).average().getAsDouble();
     }
 
     public boolean isIncludedInEvenIndex(List<Integer> arrayList, Integer specialElment) {
         List<Integer> evenList = arrayList.stream().filter(i -> i % 2 == 0).collect(Collectors.toList());
-        if (evenList.contains(specialElment)) {
-            return true;
-        } else {
-            return false;
-        }
+        return evenList.contains(specialElment);
     }
 
     public List<Integer> getUnrepeatedFromEvenIndex(List<Integer> arrayList) {
-        List<Integer> evenList = arrayList.stream().filter(i -> i % 2 == 0).collect(Collectors.toList());
-        return evenList.stream().distinct().collect(Collectors.toList());
+        return arrayList.stream().filter(i -> i % 2 == 0).distinct().collect(Collectors.toList());
     }
 
     public List<Integer> sortByEvenAndOdd(List<Integer> arrayList) {
